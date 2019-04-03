cc52f04ded5b9d02ffb5e64800f8dcc855460b71
PolinaAlexandr <polinaalexnder@gmail.com>
2019-04-03 14:30:52 +0300

    Added hard_task with single-file description

--- 05_kv/hard_one.erl
+++ 05_kv/hard_one.erl
@@ -1,5 +1,6 @@
 -module(hard_one).
--export([list_users/0]).
+-export([list_users/0, group_by_gender/1, criteriafunc_age/1, criteriafunc_gender/1, group_by/2]).
+
 
 list_users() ->
         [
@@ -17,12 +18,42 @@ group_by_gender_task1(Users) ->
             Female_Users = maps:get(female, FutureMap),
             Male_Users = maps:get(male, FutureMap),
             if
-                Users_Gender = female -> maps:update(female, [User|Female_Users], FutureMap);
-                Users_Gender = male -> maps:update(male, [User|Male_Users], FutureMap);
+                Users_Gender == female -> maps:update(female, [User|Female_Users], FutureMap);
+                Users_Gender == male -> maps:update(male, [User|Male_Users], FutureMap)
         
             end
         end,
         #{female => [], male => []}, Users),
-    FutureMap = Map,
-    Map.
+    FutureMap.
+
+
+criteriafunc_age(User) ->
+    {user, _Name, Age, _Users_Gender} = User, 
+    if 
+        Age > 0, Age =< 12 -> child;
+        Age > 12, Age =< 18 -> teeneage;
+        Age > 18, Age =< 25 -> young; 
+        Age > 25, Age =< 60 -> adult;
+        Age > 60 -> old
+    end. 
+
+criteriafunc_gender(User) ->
+    {user, _Name, _Age, Users_Gender} = User,
+    Users_Gender.
+
+
+group_by(CriteriaFun, Users) ->
+        lists:foldl(
+            fun(User, Acc) ->
+                Category = CriteriaFun(User),
+                case maps:find(Category, Acc) of 
+                    error -> maps:put(Category, [User], Acc);
+                    {ok,Values} -> maps:put(Category,[User | Values], Acc)
+                end
+            end,
+            #{},
+            Users).
+    
+
+
 
