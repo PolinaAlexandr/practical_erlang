cc1f39ce2782841280ae7bc6fd75c2515a90d38b
PolinaAlexandr <polinaalexnder@gmail.com>
2019-04-22 19:35:25 +0300

    Added lesson16 classwork part two

--- 16_sockets/lesson16.erl
+++ 16_sockets/lesson16.erl
@@ -1,34 +1,35 @@
 -module(lesson16).
--export([start/0, start/1, start_server/1, start_acceptor/1]).
+-export([start/0, start/2, start_server/2, start_acceptor/2]).
 
 start() ->
-    start(1234).
+    start(1234, 2).
 
-start(Port) ->
-    spawn(?MODULE, start_server, [Port]).
+start(Port, NumAcceptor) ->
+    spawn(?MODULE, start_server, [Port, NumAcceptor]).
 
-start_server(Port) ->
+start_server(Port, NumAcceptor) ->
     io:format("starting ~p~n", [Port]),
     {ok, ListenSocket} = gen_tcp:listen(Port, [list, {active, true}]),
-    spawn(?MODULE, start_acceptor, [ListenSocket]),
+    [spawn(?MODULE, start_acceptor, [ID, ListenSocket]) || ID <- lists:seq(1, NumAcceptor)],
     timer:sleep(infinity),
     ok.
 
-start_acceptor(ListenSocket) ->
-    io:format("Acceptor ~p ~p~n", [self(), ListenSocket]),
+start_acceptor(ListenSocket, ID) ->
+    io:format("Acceptor ~p ~p~n", [self(), ID]),
     io:format("Waiting ~n"),
     {ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
     io:format("Got client ~p ~n", [AcceptSocket]),
     % io:format("ok, take masseges ~p~n", [AcceptSocket]),
-    spawn(?MODULE, start_acceptor, [ListenSocket]),
-    handle_connect(AcceptSocket).
-handle_connect(AcceptSocket) ->
+    handle_connect(ID, ListenSocket).
+
+handle_connect(ID, ListenSocket) ->
     receive
-        {tcp, _, Msg} -> 
-            io:format("Hi, ~p ~n", [Msg]),
-            gen_tcp:send(AcceptSocket, "Ok ~n", [Msg]),
-            handle_connect(AcceptSocket);
+        {tcp, AcceptSocket, Msg} -> 
+            io:format("~p:~p: ur msg:~p~n", [self(), ID, Msg]),
+            gen_tcp:send(AcceptSocket, "Ok ~n" ++ integer_to_list(ID) ++ "." ++ Msg),
+            handle_connect(ID, ListenSocket);
         {tcp_closed, _} ->
             io:format("Connection closed ~n"),
-            ok
+            start_acceptor(ID, ListenSocket)
+            
     end.
