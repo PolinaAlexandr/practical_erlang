cef3ae86bbd86eb6ffcc1441da563022b065679c
PolinaAlexandr <polinaalexnder@gmail.com>
2019-04-22 13:17:29 +0300

    Added task9

--- 09_gen_server/exercise/chat_room_manager.erl
+++ 09_gen_server/exercise/chat_room_manager.erl
@@ -3,7 +3,7 @@
 -export([start/0,
          create_room/2, remove_room/2, get_rooms/1,
          add_user/3, remove_user/3, get_users_list/2,
-         send_message/4,  get_messages_history/2, stop/1, loop/1]).
+         send_message/4,  get_messages_history/2, stop/1, loop/1, handle_call/3]).
 
 -record(room, {room_name = [], users = [], messages = []}).
 
@@ -75,7 +75,7 @@ handle_call({create_room, RoomName}, Rooms) ->
     if 
         map_size(Rooms) >5 -> {{error, out_of_limit}, Rooms};
         map_size(Rooms) =< 5 -> RoomId = rand:uniform(100),
-        New_Room = Rooms#{RoomId => #room{room_name = RoomName}},
+        not_existing_room = Rooms#{RoomId => #room{room_name = RoomName}},
         {{ok, RoomId}, New_Room}
     end;
 
@@ -94,8 +94,45 @@ handle_call({add_user, RoomId, UserName}, Rooms)->
         {ok, Values} -> Users = Values#room.users,
     case lists:member(UserName, Users) of
         true ->  {{error, user_is_in_room}, Rooms};
-        false -> Rooms2 = Rooms#{RoomId => Values#room{ users = [UserName | Users]}},
-                 {ok, Rooms2}
+        false -> New_Room = Rooms#{RoomId => Values#room{ users = [UserName | Users]}},
+                 {ok, New_Room}
     end
 end;
+handle_call({remove_user, RoomId, UserName}, Rooms)->
 
+  case maps:find(RoomId, Rooms) of
+    error -> {{error, room_not_found},Rooms};
+    {ok, Values} -> Users = Values#room.users,
+              case lists:member(UserName, Users) of
+                false ->  {{error, user_not_in_room}, Rooms};
+                true -> New_Room = Rooms#{RoomId => Values#room{users = lists:delete(UserName,Users)}},
+                {ok, New_Room}
+              end
+    end;
+handle_call({get_users_list, RoomId}, Rooms)->
+  case maps:find(RoomId, Rooms) of
+    error -> {{error, room_not_found},Rooms};
+    {ok, Values} -> {{ok, Values#room.users}, Rooms}
+  end;
+
+handle_call({send_message, RoomId, UserName, Message}, Rooms)->
+  case maps:find(RoomId, Rooms) of
+    error -> {{error, not_existing_room},Rooms};
+    {ok, Values} -> {Users, Messages} = {Values#room.users, Values#room.messages},
+      case lists:member(UserName, Users) of
+        false ->  {{error, user_not_in_room}, Rooms};
+        true ->
+          [User] = lists:filter(fun(User)-> case User of
+            UserName -> true;
+            _ -> false
+            end end, Users),
+            New_Room = Rooms#{RoomId => Values#room{messages = [{User, Message}|Messages]}},
+          {ok, New_Room}
+      end
+  end;
+
+handle_call({get_messages_history, RoomId}, Rooms)->
+  case maps:find(RoomId, Rooms) of
+    error -> {{error, not_existing_room},Rooms};
+    {ok, Values} -> Messages = Values#room.messages, {{ok, Messages}, Rooms}
+  end.
