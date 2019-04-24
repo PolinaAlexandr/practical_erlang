ef07303956bfdf8064d1e9ac803353f9bb30a03e
PolinaAlexandr <polinaalexnder@gmail.com>
2019-04-24 16:07:53 +0300

    Modified lesson16 server

--- 16_sockets/lesson16.erl
+++ 16_sockets/lesson16.erl
@@ -1,15 +1,16 @@
 -module(lesson16).
--export([start/0, start/2, start_server/2, start_acceptor/2]).
+-export([start/0, start/2,start_server/2, start_acceptor/2]).
 
 start() ->
     start(1234, 2).
+
 
 start(Port, NumAcceptor) ->
     spawn(?MODULE, start_server, [Port, NumAcceptor]).
 
 start_server(Port, NumAcceptor) ->
     io:format("starting ~p~n", [Port]),
-    {ok, ListenSocket} = gen_tcp:listen(Port, [list, {active, true}]),
+    {ok, ListenSocket} = gen_tcp:listen(Port, [binary, {active, false}]),
     [spawn(?MODULE, start_acceptor, [ID, ListenSocket]) || ID <- lists:seq(1, NumAcceptor)],
     timer:sleep(infinity),
     ok.
@@ -20,16 +21,17 @@ start_acceptor(ListenSocket, ID) ->
     {ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
     io:format("Got client ~p ~n", [AcceptSocket]),
     % io:format("ok, take masseges ~p~n", [AcceptSocket]),
-    handle_connect(ID, ListenSocket).
+    handle_connect(ID, ListenSocket, AcceptSocket).
 
-handle_connect(ID, ListenSocket) ->
-    receive
-        {tcp, AcceptSocket, Msg} -> 
-            io:format("~p:~p: ur msg:~p~n", [self(), ID, Msg]),
-            gen_tcp:send(AcceptSocket, "Ok ~n" ++ integer_to_list(ID) ++ "." ++ Msg),
-            handle_connect(ID, ListenSocket);
-        {tcp_closed, _} ->
-            io:format("Connection closed ~n"),
+handle_connect(ID, ListenSocket, AcceptSocket) ->
+    case gen_tcp:recv(AcceptSocket, 2) of
+        {ok, Msg} -> 
+            {ok, Msg} = gen_tcp:recv(AcceptSocket, 0),
+            io:format("~p ~p: help: ~p~n ", [self(), ID, Msg]),
+            BinID = list_to_binary(integer_to_list(ID)),
+            gen_tcp:send(AcceptSocket, <<"ECHO", BinID/binary, ".", Msg/binary>>),
+            handle_connect(ID, ListenSocket, AcceptSocket);
+        {error, closed} ->
+            io:format("Connection closed:~n"),
             start_acceptor(ID, ListenSocket)
-            
     end.
